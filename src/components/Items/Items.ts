dab0936e25cb09167c9a92de4d7617518d91e5e8
Sergey Vlasov <svlasov@dtln.ru>
2022-02-17 19:02:40 +0300

    sprint_2 - забыл компонент

--- src/components/Items/Items.ts
+++ src/components/Items/Items.ts
@@ -1,19 +1,32 @@
+import { Block } from '../../utils/Block/Block';
 import { Templator } from '../../utils/Template-engine/templater';
 import { template } from './items.tmpl';
 
+type TProps = {
+	items: string;
+	className: string;
+}
+
+interface IItems {
+	props: TProps;
+	render(): ChildNode | HTMLElement | HTMLElement[];
+}
+
 const itemsTmpl = new Templator(template);
 
-class Items {
-	constructor(props) {
+class Items extends Block implements IItems {
+	props: TProps;
+
+	constructor(props: TProps) {
+		super('li', props);
 		this.props = props;
 	}
 
 	render() {
-
 		const { className, items } = this.props;
 
 		if (Array.isArray(items)) {
-			const liList = [];
+			const liList: HTMLElement[] = [];
 			items.map(el => {
 				liList.push(
 					itemsTmpl.compile({
--- src/components/Items/items.tmpl.ts
+++ src/components/Items/items.tmpl.ts
@@ -1,3 +1,3 @@
-export const template = `
+export const template: string = `
 	<li class="{% className %} item">{% items %}</li>
 `;
