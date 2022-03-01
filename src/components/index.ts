92a6636197e93116cbb2ea619d52ef2e1ebf3452
Сергей Власов <sergejvlasov@MacBook-Pro-Sergej.local>
2022-03-01 09:33:14 +0300

    sprint_2 - fix

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import { Avatar } from './Avatar/Avatar';
+import { AvatarMini } from './AvatarMini/AvatarMini';
+import { Button } from './Button/Button';
+import { Element } from './Element/Element';
+import { Input } from './input/Input';
+import { InputWithLabel } from './InputWithLabel/InputWithLabel';
+import { Items } from './Items/Items';
+import { LinkButton } from './LinkButton/LinkButton';
+import { Title } from './Title/Title';
+
+export { Avatar, AvatarMini, Button, Element, Input, InputWithLabel, Items, LinkButton, Title };
