a7284b8a628bb94f84c2f8db4c50abfc7f4f73d5
Sergey Vlasov <svlasov@dtln.ru>
2022-03-08 18:54:07 +0300

    sprint_3 - Routing init

---
+++ src/utils/Routing/Route.ts
@@ -0,0 +1,50 @@
+import { isEqual } from '../isEqual/isEqual';
+import { render } from '../renderDOM';
+import { Block } from '../Block/Block';
+
+type TBlockConstructor = new (
+	props?: Record<string, string | boolean>
+) => Block;
+
+class Route {
+	_pathname: string;
+	_blockClass: TBlockConstructor;
+	_block: Block | null;
+	_props: Record<string, any>;
+
+	constructor(pathname: string, view: TBlockConstructor, props: Record<string, any>) {
+		this._pathname = pathname;
+		this._blockClass = view;
+		this._block = null;
+		this._props = props;
+	}
+
+	protected navigate(pathname: string): void {
+		if (this.match(pathname)) {
+			this._pathname = pathname;
+			this.render();
+		}
+	}
+
+	leave(): void {
+		if (this._block) {
+			this._block.hide();
+		}
+	}
+
+	match(pathname: string): boolean {
+		return isEqual(pathname, this._pathname);
+	}
+
+	protected render(): void {
+		if (!this._block) {
+			this._block = new this._blockClass();
+			render(this._props.rootQuery, this._block);
+			return;
+		}
+
+		this._block.show();
+	}
+}
+
+export { Route, TBlockConstructor };
---
+++ src/utils/Routing/Router.ts
@@ -0,0 +1,71 @@
+import { Route, TBlockConstructor } from './Route';
+
+interface IRouter {
+	rootQuery: string;
+	routes: Route[];
+	history: History;
+	_currentRoute: Route | null;
+	_rootQuery: string;
+}
+
+class Router implements IRouter {
+	history: History;
+	routes: Route[];
+	_currentRoute: Route | null;
+	static __instance: any;
+	_rootQuery: string;
+	rootQuery: string;
+
+	constructor(rootQuery?: string) {
+		if (Router.__instance) {
+			return Router.__instance;
+		}
+
+		this.routes = [];
+		this.history = window.history;
+		this._currentRoute = null;
+		this._rootQuery = rootQuery!;
+
+		Router.__instance = this;
+	}
+
+	protected use(pathname: string, block: TBlockConstructor): this {
+		const route = new Route(pathname, block, { rootQuery: this._rootQuery });
+
+		this.routes.push(route);
+		return this;
+	}
+
+	protected start() {
+		window.onpopstate = (event: any): void => {
+			this._onRoute(event.currentTarget.location.pathname);
+		};
+
+		this._onRoute(window.location.pathname);
+	}
+
+	private _onRoute(pathname: string): void {
+		const route = this.getRoute(pathname);
+
+		if (!route) {
+			return;
+		}
+
+		if (this._currentRoute) {
+			this._currentRoute.leave();
+		}
+
+		route.render(route, pathname);
+	}
+
+	protected go(pathname: string): void {
+		this.history.pushState({}, "", pathname);
+		this._onRoute(pathname);
+	}
+
+	protected getRoute(pathname: string): any {
+		return this.routes.find(route => route.match(pathname));
+	}
+}
+
+export default Router;
